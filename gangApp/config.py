68674f207585aba9a3c0ae4499ba5f9c067d73fd
Chris-Gkikas <46613252+Chris-Gkikas@users.noreply.github.com>
2019-01-12 05:06:37 +0200

    Add files via upload

---
+++ gangApp/config.py
@@ -0,0 +1,34 @@
+# config.py
+
+# configurations for development (building the app) and production (on deployment)
+
+
+class Config(object):
+    """
+    Common configurations
+    """
+
+    # Put any configurations here that are common across all environments
+
+
+class DevelopmentConfig(Config):
+    """
+    Development configurations
+    """
+
+    DEBUG = True
+    SQLALCHEMY_ECHO = True
+
+
+class ProductionConfig(Config):
+    """
+    Production configurations
+    """
+
+    DEBUG = False
+
+
+app_config = {
+    'development': DevelopmentConfig,
+    'production': ProductionConfig
+}
---
+++ gangApp/instance/config.py
@@ -0,0 +1,6 @@
+# instance/config.py
+
+# configuration variables that will not be pushed to version control due to their sensitive nature.
+
+SECRET_KEY = 'p9Bv<3Eid9%$i01'
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://gangsAdmin:gangsAdmin@localhost/gangs_db'
